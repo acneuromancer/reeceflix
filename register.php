6b067bab68e12b9c6f9abc8dccc66cfc4b6c9efa
acneuromancer <dokukodo108@gmail.com>
2019-12-27 13:36:05 +0100

    Finish styling the login form.

--- register.php
+++ register.php
@@ -14,6 +14,11 @@
   <body>
     <div class="signInContainer">
       <div class="column">
+        <div class="header">
+          <img src="assets/images/logo.png" title="Logo" alt="Site logo">
+          <h3>Sign Up</h3>
+          <span>to continue to Reeceflix</span>
+        </div>
         <form class="" method="POST">
           <input type="text" name="firstName" placeholder="First Name" required>
           <input type="text" name="lastName" placeholder="Last Name" required>
@@ -24,6 +29,7 @@
           <input type="password" name="password2" placeholder="Confirm password" required>
           <input type="submit" name="submitButton" value="SUBMIT">
         </form>
+        <a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
       </div>
     </div>
   </body>
