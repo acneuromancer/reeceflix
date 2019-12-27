bd1c59308d3324f443c337bdbe4ef94a767a56b9
acneuromancer <dokukodo108@gmail.com>
2019-12-27 18:42:46 +0100

    Add styling the error messages.

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -4,7 +4,7 @@ class Constants {
     public static $lastNameCharacters = "Your last name must be between 2 and 25 characters.";
     public static $usernameCharacters = "Your username must be between 2 and 25 characters.";
     public static $usernameTaken = "Username already in use.";
-    public static $emailsDontMatch = "Your emails dont match.";
+    public static $emailsDontMatch = "Your emails don't match.";
     public static $emailInvalid = "Invalid email.";
     public static $emailTaken = "Email already in use.";
     public static $passwordsDontMatch = "Passwords don't match.";
