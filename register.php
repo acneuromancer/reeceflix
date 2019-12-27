29e1c753f2fc4df982a2479e80b48740b34c0682
acneuromancer <dokukodo108@gmail.com>
2019-12-27 18:35:56 +0100

    Add error handling of the register.php fields.

--- register.php
+++ register.php
@@ -1,5 +1,10 @@
 <?php
+require_once("includes/config.php");
 require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Account.php");
+require_once("includes/classes/Constants.php");
+
+$account = new Account($con);
 
 if (isset($_POST["submitButton"])) {
   $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
@@ -10,38 +15,58 @@ if (isset($_POST["submitButton"])) {
   $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
   $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
 
-  echo $firstName . "<br>";
-  echo $lastName;
+  $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
   <head>
     <meta charset="utf-8">
     <title>Welcome to Reeceflix!</title>
     <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
   </head>
+  
   <body>
     <div class="signInContainer">
+      
       <div class="column">
+        
         <div class="header">
           <img src="assets/images/logo.png" title="Logo" alt="Site logo">
           <h3>Sign Up</h3>
           <span>to continue to Reeceflix</span>
         </div>
-        <form class="" method="POST">
+        
+        <form method="POST">
+          <?php echo $account->getError(Constants::$firstNameCharacters); ?>
           <input type="text" name="firstName" placeholder="First Name" required>
+          
+          <?php echo $account->getError(Constants::$lastNameCharacters); ?>
           <input type="text" name="lastName" placeholder="Last Name" required>
-          <input type="text" name="userName" placeholder="Username" required>
+
+          <?php echo $account->getError(Constants::$usernameCharacters); ?>
+          <?php echo $account->getError(Constants::$usernameTaken); ?>
+          <input type="text" name="username" placeholder="Username" required>
+          
+          <?php echo $account->getError(Constants::$emailsDontMatch); ?>
+          <?php echo $account->getError(Constants::$emailInvalid); ?>
+          <?php echo $account->getError(Constants::$emailTaken); ?>
           <input type="email" name="email" placeholder="Email" required>
           <input type="email" name="email2" placeholder="Confirm email" required>
+
+          <?php echo $account->getError(Constants::$passwordsDontMatch); ?>
+          <?php echo $account->getError(Constants::$passwordLength); ?>
           <input type="password" name="password" placeholder="Password" required>
           <input type="password" name="password2" placeholder="Confirm password" required>
           <input type="submit" name="submitButton" value="SUBMIT">
         </form>
         <a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
       </div>
+
     </div>
+
   </body>
+
 </html>
