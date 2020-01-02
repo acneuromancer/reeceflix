2ba0dc1468669d57b662ac95ee449c3913ee47ca
acneuromancer <dokukodo108@gmail.com>
2020-01-02 17:28:18 +0000

    Pass data in the ajax call.

--- ajax/addDuration.php
+++ ajax/addDuration.php
@@ -1,4 +1,12 @@
-<?php 
-    echo "Hello";
+<?php
+require_once("../includes/config.php");
+
+if (isset($_POST["videoId"]) && isset($_POST["username"])) {
+
+} else {
+    echo "No videoId or username passed into file.";
+}
+   
+
 
 ?>
