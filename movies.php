175809f4219a8d4090acb45a65894e1a67195692
acneuromancer <dokukodo108@gmail.com>
2020-01-06 22:49:13 +0000

    Add movies page categories.

--- movies.php
+++ movies.php
@@ -5,9 +5,5 @@ $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createMoviesPreviewVideo();
 
 $containers = new CategoryContainers($con, $userLoggedIn);
-echo $containers->showTVShowCategories();
-
-
-
-
+echo $containers->showMovieCategories();
 ?>
